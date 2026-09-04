6df5aa54ebc9edb8e5271379b22ca84178210ce8
Bhavishyakumar2006 <bhavishyakumar32c@gmail.com>
2026-09-05 00:23:35 +0530

    learn about union & any

--- src/Union & Any.ts
+++ src/Union & Any.ts
@@ -1,5 +1,24 @@
 //union
-
 let subs: string | number = '1M';
 
 let airlineSeat: 'aisel' | 'middle' | 'window' = 'window';
+airlineSeat = 'aisel'
+
+let apiRequest: 'pending' | 'success' | 'error' = 'success'
+
+let name;
+
+//any
+const orders = ['10', '15', '25' , '30'];
+
+// TIP -: Don't use any
+
+let currentOrder: string | undefined;
+
+for (const order of orders) {
+    if (order === '30') {
+        currentOrder = order;
+    }
+}
+
+console.log(currentOrder);
